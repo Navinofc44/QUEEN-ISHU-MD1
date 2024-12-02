e7c5f87c8a785b7ce43f2177a05b11a22a79b26c
Navinofc44 <Pahasarasanujja@gmail.com>
2024-12-02 20:27:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 ///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!MAIN-CONFIG=================================
-SESSION_ID: process.env.SESSION_ID || "Put Session Id",  // ADD Your Session Id 
+SESSION_ID: process.env.SESSION_ID || "nVRA0JiI#Oeqn4q6gWjm7SiO0Z76HdKm2FsyQKoG4jD_fHgnDu8U",  // ADD Your Session Id 
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
-BOT_NAME: process.env.BOT_NAME || "laksidu",
+BOT_NAME: process.env.BOT_NAME || "Pahasara Tm",
 ///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!CUSTOM-SETTING-CONFIG=====================
 AUTO_VOICE: process.env.AUTO_VOICE || "true", //add true or false
 AUTO_STICKER: process.env.AUTO_STICKER || "true", //add true or false
@@ -27,5 +27,5 @@ PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJ
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 ///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!ALIVE-CONFIG================================
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/vzDwkjg.jpeg", //add alive img url
-ALIVE_MSG: process.env.ALIVE_MSG || "HI AME WHATSAPP BOT", //add alive msg
+ALIVE_MSG: process.env.ALIVE_MSG || "𝗦𝗣𝗘𝗘𝗗 𝘄𝗵𝗮𝘁𝘀𝗮𝗽𝗽 𝗕𝗢𝗧 𝗰𝗿𝗲𝘁𝗲𝗱 𝗯𝘆 𝗟𝗔𝗞𝗦𝗜𝗡𝗗𝗨 🌝♥️🎀", //add alive msg
 };
